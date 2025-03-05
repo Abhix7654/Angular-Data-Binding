80c04c6233ecfafc5b01954f10c2ed8888d901d9
abhishek <rajabhishek7654@gmail.com>
2025-03-05 19:13:52 +0530

    [Abhishek] add. Angular Ability To display Hello from BridgeLabz with a user inputted name Two Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
 
   imgUrl = "assets/BL_logo_square_jpg.jpg"; 
   url ="https://www.bridgelabz.com";
+  nameError: string = "";
+
 userName: string = "";
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz";
@@ -19,4 +21,14 @@ userName: string = "";
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+  onInput($event:any){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = /^[A-Z]{1}[a-zA-Z\\s]{2,}$/;
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    } 
+      this.nameError = "Name is Incorrect!";
+  
+    }
 }
